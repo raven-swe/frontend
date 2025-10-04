1f4cfe9e88e0d7393ce4d7e6aefaca93d7ad38ac
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-04 16:31:04 +0300

    feat: add pinia to nuxt with example store

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
 import Button from '@/components/ui/Button.vue';
+const counter = useCounterStore();
 </script>
 
 <template>
   <div>
     <h1 class="text-2xl p-4">
-      {{ $t('welcome') }}
+      {{ $t('welcome') + ` ${counter.count}` }}
     </h1>
-    <Button>{{ $t('testButton') }}</Button>
+    <Button @click="counter.increment">{{ $t('testButton') }}</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n',
     '@nuxt/test-utils/module',
     '@vueuse/nuxt',
+    '@pinia/nuxt',
   ],
   vite: {
     plugins: [tailwindcss()],
