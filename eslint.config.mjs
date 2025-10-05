4e28b68e1a03be9fbe82b158a79ced19d3eaf0cd
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-06 00:51:52 +0300

    build(eslint): add custom regex rule to disallow ltr classes in Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,24 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
+import pluginRegex from 'eslint-plugin-regex';
 
 export default withNuxt({
+  ignores: ['eslint.config.mjs', '.output', '.nuxt', 'node_modules'],
+  plugins: {
+    regex: pluginRegex,
+  },
   rules: {
     'vue/no-bare-strings-in-template': 'error',
+    'regex/invalid': [
+      'error',
+      [
+        {
+          id: 'no-ltr-tailwind',
+          message:
+            'Avoid left/right-based Tailwind classes. Use logical ones (ms/me, ps/pe, start/end).',
+          regex: String.raw`(?<=\bclass(?:Name)?=["'][^"']*)\b(?:ml|mr|pl|pr|left|right|inset-(?:l|r))(?:-[^\s"'>]+)?\b`,
+        },
+      ],
+    ],
   },
 });
