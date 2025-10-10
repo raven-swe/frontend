4eefa5a615e396c7d527e43d776b52ac64d22122
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-11 01:02:04 +0300

    feat: update test page to show button variants

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,10 +5,23 @@ const counter = useCounterStore();
 </script>
 
 <template>
-  <div>
+  <div class="p-8">
     <h1 class="p-4 text-2xl">
       {{ $t('welcome') + ` ${counter.count}` }}
     </h1>
-    <Button @click="counter.increment">{{ $t('testButton') }}</Button>
+    <div class="flex flex-col gap-2">
+      <Button class="w-75" variant="default" @click="counter.increment">{{
+        $t('testButton')
+      }}</Button>
+      <Button class="w-75" variant="outline" @click="counter.increment">{{
+        $t('testButton')
+      }}</Button>
+      <Button variant="ghost-default" class="w-75">
+        {{ $t('testButton') }}
+      </Button>
+      <Button variant="ghost-primary" size="icon">
+        <Icon name="majesticons:image-line" />
+      </Button>
+    </div>
   </div>
 </template>
