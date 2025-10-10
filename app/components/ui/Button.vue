7041a13719d6ae182d81543e4480f07771c49688
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-11 01:01:24 +0300

    feat: add ghost-primary style for button and update size for icons

--- app/components/ui/Button.vue
+++ app/components/ui/Button.vue
@@ -2,14 +2,20 @@
 import { cva } from 'class-variance-authority';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-md  font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-ring focus-visible:ring-[2px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-md  font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-5 shrink-0 [&_svg]:shrink-0 outline-none  focus-visible:ring-[2px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
-          'bg-foreground text-background hover:bg-foreground/90 focus-visible:bg-foreground/90',
+          'bg-foreground text-background hover:bg-foreground/90 focus-visible:bg-foreground/90 focus-visible:ring-ring',
         outline:
-          'border-input border-1 bg-background hover:bg-foreground/10 focus-visible:bg-foreground/10',
+          'border-input border-1 bg-background hover:bg-foreground/10 focus-visible:bg-foreground/10 focus-visible:ring-ring',
+
+        'ghost-default':
+          'bg-background hover:bg-foreground/10 focus-visible:bg-foreground/10 focus-visible:ring-ring',
+
+        'ghost-primary':
+          'text-primary hover:bg-primary/10 focus-visible:bg-primary/10 focus-visible:ring-ring-primary',
       },
       size: {
         default: 'h-10 px-4 py-2 has-[>svg]:px-3',
