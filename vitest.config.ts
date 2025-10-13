74615c82d9ab7ff706f118a5e2bf3f1977f28de7
habibayman <habibaayman2004@outlook.com>
2025-10-13 20:10:10 +0300

    chore: config vitest to remove playgrounds from the coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       reporter: ['text', 'json', 'lcov'],
       reportsDirectory: './coverage',
       include: ['app/**'],
+      exclude: ['**/pages/playground/**'],
     },
     projects: [
       {
