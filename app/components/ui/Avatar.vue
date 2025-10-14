21c92ea310cc2232992da0322f59cfc2a7242dd3
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-14 18:14:35 +0300

    feat: add generic avatar which supports default and primary variants and sizes

---
+++ app/components/ui/Avatar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { cva } from 'class-variance-authority';
+import { AvatarRoot, AvatarImage, AvatarFallback } from 'reka-ui';
+
+const avatarRoot = cva('relative flex shrink-0 overflow-hidden rounded-full', {
+  variants: {
+    variant: {
+      default: 'cursor-default',
+      primary: 'cursor-pointer hover:brightness-96',
+    },
+    size: {
+      sm: 'size-10',
+      md: 'size-16',
+      lg: 'size-24',
+      xl: 'size-92',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+});
+
+interface Props {
+  variant?: NonNullable<Parameters<typeof avatarRoot>[0]>['variant'];
+  size?: NonNullable<Parameters<typeof avatarRoot>[0]>['size'];
+  img?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'primary',
+  size: 'md',
+  img: 'https://placehold.co/100x100',
+});
+</script>
+
+<template>
+  <AvatarRoot :class="avatarRoot({ variant, size })">
+    <AvatarImage class="aspect-square size-full" :src="img" alt="User Avatar" />
+    <AvatarFallback class="bg-muted flex size-full items-center justify-center rounded-full" />
+  </AvatarRoot>
+</template>
+
+<style scoped></style>
---
+++ app/pages/playground/avatars.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Avatar from '@/components/ui/Avatar.vue';
+</script>
+
+<template>
+  <div>
+    <div class="p-4">
+      <Avatar :img="'https://placehold.co/40x40'" size="sm" />
+    </div>
+    <div class="p-4">
+      <Avatar :img="'https://placehold.co/64x64'" size="md" />
+    </div>
+    <div class="p-4">
+      <Avatar :img="'https://placehold.co/96x96'" size="lg" />
+    </div>
+    <div class="p-4">
+      <Avatar :img="'https://placehold.co/368x368'" size="xl" variant="default" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
