bc9923f5b88cce4c03589390dc9d2dfd8390edf5
habibayman <habibaayman2004@outlook.com>
2025-10-16 20:18:14 +0300

    feat(generic-tabs): creates tabs wrapper

--- app/components/ui/Tab.vue
+++ app/components/ui/Tab.vue
@@ -1,11 +1,14 @@
 <template>
   <div
-    class="hover:hover:bg-foreground/10 flex h-[53px] items-center justify-center px-4 text-[15px] transition-colors"
+    class="hover:hover:bg-foreground/10 flex h-[53px] cursor-pointer items-center justify-center px-4 text-[15px] transition-colors"
     :class="[isActive ? 'text-foreground font-bold' : 'text-muted-foreground font-medium']"
   >
     <NuxtLink :to="route" class="relative flex h-full items-center">
       {{ label }}
-      <div v-if="isActive" class="bg-primary absolute start-0 bottom-0 h-0.5 w-full"></div>
+      <div
+        v-if="isActive"
+        class="bg-primary absolute start-0 bottom-0 h-1 w-full rounded-b-md"
+      ></div>
     </NuxtLink>
   </div>
 </template>
