e3ce91f3d86a6ea75230de8f1715db5e7ce65503
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-17 00:07:01 +0300

    feat: add shadcn hover card component

---
+++ app/components/ui/hover-card/HoverCardTrigger.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { HoverCardTriggerProps } from 'reka-ui';
+import { HoverCardTrigger } from 'reka-ui';
+
+const props = defineProps<HoverCardTriggerProps>();
+</script>
+
+<template>
+  <HoverCardTrigger
+    class="inline-block cursor-pointer rounded-full outline-none"
+    data-slot="hover-card-trigger"
+    v-bind="props"
+  >
+    <slot />
+  </HoverCardTrigger>
+</template>
