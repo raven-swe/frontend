20c0d879c7e97b2e8f6e9e9bf8f27f0e9da27fb2
habibayman <habibaayman2004@outlook.com>
2025-10-16 17:27:47 +0300

    feat: init Tab component

---
+++ app/components/ui/Tab.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLink
+    :to="route"
+    class="text-muted-foreground hover:bg-accent relative flex h-[53px] items-center justify-center px-4 text-[15px] font-medium transition-colors"
+    :class="{
+      'text-foreground font-bold': isActive,
+      'after:bg-primary after:absolute after:right-0 after:bottom-0 after:left-0 after:h-[4px] after:rounded-full':
+        isActive,
+    }"
+  >
+    {{ label }}
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  route: string;
+  isActive: boolean;
+}>();
+</script>
