30ca2a319e9a16e872d8ee9f229e77d6851a6a0d
Ahmed Amr <43810060+AhmedAmrNabil@users.noreply.github.com>
2025-10-17 10:42:20 +0300

    feat: styled dialog - [CU-869aqj2e6] (#24)

---
+++ app/components/ui/dialog/DialogFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/utils';
+
+const props = defineProps<{ class?: HTMLAttributes['class'] }>();
+</script>
+
+<template>
+  <div
+    data-slot="dialog-footer"
+    :class="cn('flex h-fit flex-col items-center justify-center gap-4', props.class)"
+  >
+    <slot />
+  </div>
+</template>
