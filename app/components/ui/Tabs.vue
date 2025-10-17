07859c0d60cfaafa9e05ff26daa4a2f7b35bb831
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2025-10-17 12:26:47 +0300

    feat(generic-ui): styled tabs nav component - [CU-869arj3a5] (#20)

---
+++ app/components/ui/Tabs.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="tabs border-border flex w-full border-b" aria-role="tablist">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.tabs > * {
+  flex: 1;
+  min-width: 0;
+}
+</style>
