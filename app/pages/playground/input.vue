4409938922a200bc99d92a5618fa8b1d60ac13cd
Ahmed Amr <43810060+AhmedAmrNabil@users.noreply.github.com>
2025-10-17 10:01:09 +0300

    feat: styled input - [CU-869atun8w] (#12)

---
+++ app/pages/playground/input.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Input from '~/components/ui/Input.vue';
+</script>
+
+<template>
+  <div class="p-8">
+    <!-- eslint-disable-next-line vue/no-bare-strings-in-template -->
+    <h1 class="p-4 text-2xl">Input Playground</h1>
+    <Input class="mb-4 w-64" placeholder="Name" aria-invalid="true" />
+    <Input class="w-64" placeholder="Email or Username" />
+  </div>
+</template>
