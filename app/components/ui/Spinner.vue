cfadc961c6a64d5251c9ffe9f5c639cd94c4f0a7
habibayman <habibaayman2004@outlook.com>
2025-10-17 20:20:18 +0300

    feat(generic-ui): vendor shadcn spinner

---
+++ app/components/ui/Spinner.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { Loader2Icon } from 'lucide-vue-next';
+import { cn } from '@/utils/index';
+
+const props = defineProps<{
+  class?: HTMLAttributes['class'];
+}>();
+</script>
+
+<!-- for API https://www.shadcn-vue.com/docs/components/spinner.html#spinner -->
+<template>
+  <Loader2Icon
+    role="status"
+    :aria-label="$t('ui.loading')"
+    :class="cn('size-4 animate-spin', props.class)"
+  />
+</template>
