16260299d57288d59088f2468353f30e0cd2a35d
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-17 21:20:04 +0300

    feat: add FieldInput component for vee-validate

---
+++ app/components/ui/form/FieldInput.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useField } from 'vee-validate';
+import Input from '../Input.vue';
+const props = defineProps<{ name: string; placeholder: string }>();
+const { value, errorMessage } = useField<string | number>(() => props.name);
+</script>
+
+<template>
+  <div :class="$attrs.class">
+    <Input
+      v-model="value"
+      :placeholder="placeholder"
+      :aria-invalid="!!errorMessage"
+      v-bind="$attrs"
+    />
+    <p v-if="errorMessage" class="text-destructive ps-1 text-xs">{{ errorMessage }}</p>
+  </div>
+</template>
