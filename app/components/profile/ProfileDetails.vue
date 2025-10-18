f5fdac8592adaaa2e752a983daa5d5a43a59b95a
habibayman <habibaayman2004@outlook.com>
2025-10-18 15:16:20 +0300

    test(profile-layout): write unit tests for profile details

--- app/components/profile/ProfileDetails.vue
+++ app/components/profile/ProfileDetails.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="bg-accent/10 h-[460px]">
-    <h1 class="p-4 text-2xl font-bold"></h1>
-  </div>
+  <div class="bg-accent/10 h-[460px]"></div>
 </template>
