64525bf3186e44a6871ad81bdc0a766f106d8201
Hussein Mohamed <shsmohmed4@gmail.com>
2025-10-19 16:16:18 +0300

    feat: implement user profile page with cover, avatar, and details

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -2,10 +2,22 @@
 import ProfileAvatarSection from '~/components/profile/ProfileAvatarSection.vue';
 import ProfileCover from '~/components/profile/ProfileCover.vue';
 import ProfileDetails from '~/components/profile/ProfileDetails.vue';
+definePageMeta({
+  layout: 'profile',
+});
+
+const posts = [
+  { id: 1, content: 'Post 1' },
+  { id: 2, content: 'Post 2' },
+  { id: 3, content: 'Post 3' },
+];
 </script>
 
 <template>
   <div>
+    <div v-for="post in posts" :key="post.id">
+      <p>{{ post.content }}</p>
+    </div>
     <ProfileCover />
     <ProfileAvatarSection />
     <ProfileDetails />
