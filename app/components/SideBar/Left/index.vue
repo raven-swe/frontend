8d88a3d09b9254362cacf5da2eff5a39184304e8
habibayman <habibaayman2004@outlook.com>
2025-10-19 23:57:49 +0300

    fix: merge conflicts

--- app/components/SideBar/Left/index.vue
+++ app/components/SideBar/Left/index.vue
@@ -2,13 +2,13 @@
 import { SIDEBAR_TABS } from '~/constants/leftsidebar';
 </script>
 <template>
-  <div class="flex h-screen flex-col">
+  <div class="flex h-screen flex-col items-center xl:items-start">
     <div class="hover:bg-accent my-2 w-min p-2 hover:rounded-full">
       <NuxtLink to="/">
         <LogoRaven class="h-8 w-8" />
       </NuxtLink>
     </div>
-    <div class="mt-2 space-y-3">
+    <div class="mt-2 flex flex-col items-center space-y-3 xl:items-start">
       <SideBarLeftTab v-for="tab in SIDEBAR_TABS" :key="tab.label" :tab />
     </div>
   </div>
