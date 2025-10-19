e4e26dde87ffe09edbd487862baa8d9e8bd4da85
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2025-10-20 00:06:51 +0300

    feat(generic-ui): styled spinner - [CU-869aw1b8u] (#26)

---
+++ app/components/ui/Spinner.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/utils/index';
+
+const props = withDefaults(
+  defineProps<{
+    class?: HTMLAttributes['class'];
+    size?: string | number;
+  }>(),
+  {
+    size: '2rem',
+  },
+);
+</script>
+
+<template>
+  <Icon
+    name="mingcute:loading-3-fill"
+    role="status"
+    mode="svg"
+    :size="props.size"
+    :aria-label="$t('ui.loading')"
+    :class="cn('!block animate-spin', props.class)"
+  />
+</template>
