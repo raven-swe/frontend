3ea65e19b1fee29e7fc1edaaed5811860c9d63c6
habibayman <habibaayman2004@outlook.com>
2025-10-20 10:20:43 +0300

    feat(setup-profile): location dialog skeleton

---
+++ app/components/profile/setup/LocationDialog.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import type { buttonVariants } from '~~/types/ui';
+
+const actionButton = computed(() => {
+  const isLocationSet = false;
+  return {
+    text: isLocationSet ? $t('ui.next') : $t('ui.skip-for-now'),
+    variant: (isLocationSet ? 'primary' : 'outline') as buttonVariants,
+  };
+});
+</script>
+
+<template>
+  <div>
+    <div class="p-4"></div>
+    <div class="flex flex-col items-start gap-4 p-8">
+      <UiDialog>
+        <UiDialogTrigger as-child>
+          <UiButton variant="outline" size="sm">
+            {{ $t('profile.setup.setup-profile') }}
+          </UiButton>
+        </UiDialogTrigger>
+        <UiDialogContent header-class="flex items-center justify-center p-0" class="h-auto">
+          <template #header>
+            <img src="https://placehold.co/32x32" class="size-8" />
+          </template>
+          <UiDialogHeader class="px-8 py-4">
+            <UiDialogTitle class="text-3xl font-bold">{{
+              $t('profile.setup.add-location')
+            }}</UiDialogTitle>
+            <!--  -->
+            <UiDialogDescription>
+              {{ $t('profile.setup.location-desc') }}
+            </UiDialogDescription>
+          </UiDialogHeader>
+          <div class="mx-2 flex flex-grow items-center justify-center">
+            <uiInput type="text" placeholder="location" class="w-full max-w-md" maxlength="30" />
+          </div>
+          <UiDialogFooter>
+            <UiButton :variant="actionButton.variant" class="w-100" size="xl">
+              {{ actionButton.text }}
+            </UiButton>
+          </UiDialogFooter>
+        </UiDialogContent>
+      </UiDialog>
+    </div>
+  </div>
+</template>
--- app/components/ui/Input.vue
+++ app/components/ui/Input.vue
@@ -47,6 +47,7 @@ const remainingChars = computed(() => {
         v-model="modelValue"
         class="peer file:text-foreground w-full min-w-0 outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
         v-bind="$attrs"
+        :maxlength="props.maxlength"
         @focus="focused = true"
         @blur="focused = false"
       />
