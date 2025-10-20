108f91a0563f8a8c8f8dff661d36c2caea23531e
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-20 03:54:46 +0300

    feat(mocks): add mock user generation and login handlers

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,7 +1,10 @@
+import { loginHandlers } from './handlers/login';
 import { handlers as userHandlers } from './handlers/user';
+
 // Import more handlers as needed
 
 export const handlers = [
   ...userHandlers,
+  ...loginHandlers,
   // Add More handlers here
 ];
