810424717c161a019a90e365159a14ee46e7f38e
habibayman <habibaayman2004@outlook.com>
2025-10-20 11:33:57 +0300

    feat(setup-profile): playground for discrete profile setup dialogs

---
+++ app/pages/playground/test.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProfilePictureDialog from '~/components/profile/setup/ProfilePictureDialog.vue';
+import LocationDialog from '~/components/profile/setup/LocationDialog.vue';
+import BioDialog from '~/components/profile/setup/BioDialog.vue';
+</script>
+
+<template>
+  <div>
+    <ProfilePictureDialog />
+    <LocationDialog />
+    <BioDialog />
+  </div>
+</template>
