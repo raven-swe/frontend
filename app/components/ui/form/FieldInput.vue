3c3a355a5fe7310e3d364f2b155c9b4ff532af39
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-20 04:59:51 +0300

    feat(form): add FieldInput and FieldPassword components

---
+++ app/components/ui/form/FieldInput.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useField } from 'vee-validate';
+const props = defineProps<{ name: string; placeholder: string }>();
+const { value, errorMessage } = useField<string | number>(() => props.name);
+</script>
+
+<template>
+  <div :class="$attrs.class">
+    <UiInput
+      v-model="value"
+      :placeholder="placeholder"
+      :aria-invalid="!!errorMessage"
+      v-bind="$attrs"
+    />
+    <p v-if="errorMessage" class="text-destructive ps-1 text-xs">{{ errorMessage }}</p>
+  </div>
+</template>
