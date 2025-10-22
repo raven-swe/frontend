49833fba9520a7fbe5e2e428d8f8480110983c34
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-22 17:31:33 +0300

    feat(api): implement serverApiFetch utility for API requests with error handling

---
+++ server/utils/api.ts
@@ -0,0 +1,17 @@
+import { ofetch } from 'ofetch';
+import { createError } from 'h3';
+
+const API_URL = process.env.BACKEND_URL;
+
+export const serverApiFetch = ofetch.create({
+  baseURL: API_URL,
+  credentials: 'include',
+
+  async onResponseError({ response }) {
+    throw createError({
+      statusCode: response.status || 500,
+      statusMessage: response._data?.error?.message || response._data?.message || 'Server Error',
+      data: response._data?.error || response._data,
+    });
+  },
+});
