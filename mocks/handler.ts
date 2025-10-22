ab9bb8e760c2b98f0fae4c991a34e8036649feab
habibayman <habibaayman2004@outlook.com>
2025-10-22 20:36:28 +0300

    feat(mock-server): create update profile mock handlers

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,9 +1,6 @@
 import { handlers as userHandlers } from './handlers/user';
 import { handlers as tweetHandlers } from './handlers/tweet';
+import { handlers as profileHandlers } from './handlers/update-profile';
 // Import more handlers as needed
 
-export const handlers = [
-  ...userHandlers,
-  ...tweetHandlers,
-  // Add More handlers here
-];
+export const handlers = [...userHandlers, ...tweetHandlers, ...profileHandlers];
