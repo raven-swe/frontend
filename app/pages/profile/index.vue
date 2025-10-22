9ae0d87545f46b2f6792870ce0a0b2b3fcb5bff3
Abdallah-farag27 <abdallah_altalbawy@yahoo.com>
2025-10-22 23:53:12 +0300

    feat: Added tweets to profile page

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -1,19 +1,25 @@
-<script setup>
+<script setup lang="ts">
+import type { Tweet } from '~~/shared/types/tweets';
+import TweetDefaultCard from '../../components/tweet/TweetDefaultCard.vue';
 definePageMeta({
   layout: 'profile',
 });
 
-const posts = [
-  { id: 1, content: 'Post 1' },
-  { id: 2, content: 'Post 2' },
-  { id: 3, content: 'Post 3' },
-];
+const tweets = ref<Tweet[]>([]);
+
+const { data: tweetsData, error } = await useFetch<{ data: Tweet[] }>('/api/tweets');
+tweets.value = tweetsData.value?.data || [];
+
+if (error.value) console.error(error.value);
 </script>
 
 <template>
-  <div>
-    <div v-for="post in posts" :key="post.id">
-      <p>{{ post.content }}</p>
+  <div class="p-2">
+    <div v-if="tweets.length > 0" class="mb-4">
+      <div class="mt-4 flex w-full max-w-[700px] flex-col gap-4">
+        <TweetDefaultCard v-for="tweet in tweets" :key="tweet.id" :tweet="tweet" />
+      </div>
     </div>
+    <h1 v-else>{{ $t('testing.tweets.tweet-not-found') }}</h1>
   </div>
 </template>
