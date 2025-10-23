408f4c3b08f19661a08f9bdba17c6a3ea3464ebd
habibayman <habibaayman2004@outlook.com>
2025-10-23 15:05:23 +0300

    feat(edit-profile): add sticky header section

--- app/components/ui/dialog/DialogContent.vue
+++ app/components/ui/dialog/DialogContent.vue
@@ -36,7 +36,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
       @escape-key-down="(e) => e.preventDefault()"
       @interact-outside="(e) => e.preventDefault()"
     >
-      <div class="relative flex h-13 flex-row items-center justify-start gap-2 p-2">
+      <div class="relative flex h-13 flex-shrink-0 flex-row items-center justify-start gap-2 p-2">
         <DialogClose v-if="!hideCloseButton" data-slot="dialog-close" as-child>
           <Button variant="ghost-default" size="icon-xs" class="absolute inset-2">
             <Icon name="lucide:x" class="size-5" />
@@ -50,7 +50,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
         </div>
       </div>
 
-      <div class="flex h-full w-full flex-col gap-4 px-6 pb-6">
+      <div class="flex min-h-0 w-full flex-1 flex-col gap-4 overflow-y-auto px-6 pb-6">
         <slot />
       </div>
     </DialogContent>
--- app/pages/settings/profile.vue
+++ app/pages/settings/profile.vue
@@ -11,7 +11,7 @@ const selectedImage = ref<string | null>(null);
 const selectedProfileImage = ref<string | null>(null);
 const name = ref<string>('habibayman');
 const bio = ref<string>(
-  "Fourth year Computer Engineering student @ Cairo university || GSoC25\nI'm only here when the reels get boring on ig",
+  "Fourth year Computer Engineering student @ Cairo university\nI'm only here when the reels get boring on ig",
 );
 const location = ref<string>('Cairo, Egypt');
 const website = ref<string>('https://github.com/habibayman');
@@ -49,104 +49,112 @@ const handleProfileFileChange = (event: Event) => {
     reader.readAsDataURL(file);
   }
 };
+
+const handleSubmit = () => {
+  // send all the data to be updated
+};
 </script>
 
 <template>
   <UiDialog :open="true">
     <UiDialogContent
-      header-class="flex items-center justify-center p-0"
-      class="h-auto !w-[650px] !max-w-[650px] overflow-y-auto"
+      header-class="flex items-center justify-between px-4"
+      class="h-auto !w-[650px] !max-w-[650px] !p-0"
     >
       <template #header>
-        <img src="https://placehold.co/32x32" class="size-8" />
+        <h2 class="text-xl font-bold">{{ $t('profile.edit.edit-profile') }}</h2>
+        <UiButton class="w-16" size="xs" @click="handleSubmit">{{ $t('ui.save') }}</UiButton>
       </template>
-      <UiDialogHeader class="px-8 py-4">
-        <UiDialogTitle class="text-3xl font-bold">{{
-          $t('profile.setup.pick-header')
-        }}</UiDialogTitle>
-        <UiDialogDescription>
-          {{ $t('profile.setup.header-desc') }}
-        </UiDialogDescription>
-      </UiDialogHeader>
-      <!-- Header Image Section - Full Width -->
-      <div class="relative w-full">
-        <div
-          v-if="!selectedImage"
-          class="bg-muted-foreground/50 h-40 w-full cursor-pointer"
-          @click="handleImageClick"
-        />
-        <img
-          v-else
-          :src="selectedImage"
-          class="h-40 w-full cursor-pointer object-cover brightness-70 filter"
-          @click="handleImageClick"
-        />
-        <button
-          type="button"
-          class="bg-foreground/60 hover:bg-foreground/80 absolute start-1/2 top-1/2 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full transition-colors"
-          @click="handleImageClick"
-        >
-          <Icon name="lucide:camera" class="text-white" size="1.2rem" />
-        </button>
-        <input
-          ref="fileInputRef"
-          type="file"
-          accept="image/*"
-          class="hidden"
-          @change="handleFileChange"
-        />
-      </div>
-      <!-- Profile Section -->
-      <div class="relative z-10 -mt-12 flex flex-col items-center gap-3 self-start px-3">
-        <div class="relative">
+
+      <div class="flex h-full w-full flex-col">
+        <UiDialogHeader>
+          <UiDialogTitle />
+          <UiDialogDescription />
+        </UiDialogHeader>
+
+        <!-- Header Image Section -->
+        <div class="relative w-full">
+          <div
+            v-if="!selectedImage"
+            class="bg-muted-foreground/50 h-40 w-full cursor-pointer"
+            @click="handleImageClick"
+          />
           <img
-            :src="selectedProfileImage || '/default_profile.png'"
-            class="h-30 w-30 cursor-pointer rounded-full border-3 border-white object-cover"
-            @click="handleProfileImageClick"
+            v-else
+            :src="selectedImage"
+            class="h-40 w-full cursor-pointer object-cover brightness-70 filter"
+            @click="handleImageClick"
           />
           <button
             type="button"
-            class="bg-foreground/60 hover:bg-foreground/80 absolute start-1/2 top-1/2 flex h-8 w-8 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full transition-colors"
-            @click="handleProfileImageClick"
+            class="bg-foreground/60 hover:bg-foreground/80 absolute start-1/2 top-1/2 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full transition-colors"
+            @click="handleImageClick"
           >
-            <Icon name="lucide:camera" class="text-white" size="1rem" />
+            <Icon name="lucide:camera" class="text-white" size="1.2rem" />
           </button>
           <input
-            ref="profileFileInputRef"
+            ref="fileInputRef"
             type="file"
             accept="image/*"
             class="hidden"
-            @change="handleProfileFileChange"
+            @change="handleFileChange"
           />
         </div>
+
+        <!-- Profile Section -->
+        <div class="relative z-10 -mt-12 mb-6 flex flex-col items-center gap-3 self-start px-3">
+          <div class="relative">
+            <img
+              :src="selectedProfileImage || '/default_profile.png'"
+              class="h-30 w-30 cursor-pointer rounded-full border-3 border-white object-cover"
+              @click="handleProfileImageClick"
+            />
+            <button
+              type="button"
+              class="bg-foreground/60 hover:bg-foreground/80 absolute start-1/2 top-1/2 flex h-8 w-8 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full transition-colors"
+              @click="handleProfileImageClick"
+            >
+              <Icon name="lucide:camera" class="text-white" size="1rem" />
+            </button>
+            <input
+              ref="profileFileInputRef"
+              type="file"
+              accept="image/*"
+              class="hidden"
+              @change="handleProfileFileChange"
+            />
+          </div>
+        </div>
+
+        <!-- Inputs Section -->
+        <div class="w-full px-4 pb-6">
+          <UiInput v-model="name" placeholder="Name" class="mb-6" />
+          <uiInput
+            v-model="bio"
+            type="textarea"
+            :rows="2"
+            :placeholder="$t('profile.edit.bio')"
+            class="mb-6 w-full"
+            maxlength="160"
+          />
+          <uiInput
+            v-model="location"
+            type="text"
+            :placeholder="$t('profile.edit.location')"
+            class="mb-6 w-full"
+            maxlength="30"
+          />
+          <uiInput
+            v-model="website"
+            type="text"
+            :placeholder="$t('profile.edit.website')"
+            class="mb-6 w-full"
+            maxlength="100"
+          />
+          <!-- date picker component should go here -->
+        </div>
       </div>
-      <!-- Inputs -->
-      <div class="w-full px-4">
-        <UiInput v-model="name" placeholder="Name" class="mb-6" />
-        <uiInput
-          v-model="bio"
-          type="textarea"
-          :rows="2"
-          placeholder="Bio"
-          class="mb-6 w-full"
-          maxlength="160"
-        />
-        <uiInput
-          v-model="location"
-          type="text"
-          placeholder="location"
-          class="mb-6 w-full"
-          maxlength="30"
-        />
-        <uiInput
-          v-model="website"
-          type="text"
-          placeholder="website"
-          class="mb-6 w-full"
-          maxlength="100"
-        />
-        <!-- date picker component should go here -->
-      </div>
+
       <UiDialogFooter />
     </UiDialogContent>
   </UiDialog>
