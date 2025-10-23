bba11c401adcc0248d243a08f9be86d7cb59a070
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-23 22:31:22 +0300

    refactor(auth): streamline token generation and clean up login handlers

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,6 +1,7 @@
-import { loginHandlers } from './handlers/login';
+import { handlers as loginHandlers } from './handlers/login';
 import { handlers as userHandlers } from './handlers/user';
 import { handlers as tweetHandlers } from './handlers/tweet';
+import { handlers as passwordHandlers } from './handlers/password';
 // Import more handlers as needed
 
-export const handlers = [...userHandlers, ...loginHandlers, ...tweetHandlers];
+export const handlers = [...userHandlers, ...loginHandlers, ...tweetHandlers, ...passwordHandlers];
