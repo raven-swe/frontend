cf751a3374191bd63bcd0ded7f51535ee9166008
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-10-29 21:32:48 +0300

    test: refactor check-identifier handler and fix unit tests

--- server/api/auth/check-identifier.get.ts
+++ server/api/auth/check-identifier.get.ts
@@ -1,4 +1,5 @@
-export default defineEventHandler(async (event) => {
+import { defineWrappedResponseHandler } from '~~/server/utils/handler';
+export default defineWrappedResponseHandler(async (event) => {
   const query = getQuery<{ identifier: string }>(event);
   const response = await serverApiFetch<ApiSuccessResponse<{ exists: boolean; type: string }>>(
     '/auth/check-identifier',
