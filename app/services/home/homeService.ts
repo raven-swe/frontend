1cc0a0f87c882c5d0aea36885bf56ec27efc2dd2
Mostafa Hassan <moustafahassan028@gmail.com>
2025-10-30 22:47:41 +0300

    feat(home): implement home service for fetching timelines

---
+++ app/services/home/homeService.ts
@@ -0,0 +1,37 @@
+import type { Tweet } from '~~/shared/types/tweets';
+export type timelineSchema = {
+  limit: number;
+  cursor: string | null;
+};
+export type Pagination = {
+  cursor: string | null;
+  nextCursor: string | null;
+  hasNextPage: boolean;
+};
+export const homeService = {
+  async forYou(timeline: timelineSchema) {
+    return await $fetch<ApiSuccessResponse<{ data: Tweet[]; pagination: Pagination }>>(
+      '/api/timeline/for-you',
+      {
+        method: 'GET',
+        query: {
+          limit: timeline.limit,
+          cursor: timeline.cursor,
+        },
+      },
+    );
+  },
+
+  async following(timeline: timelineSchema) {
+    return await $fetch<ApiSuccessResponse<{ data: Tweet[]; pagination: Pagination }>>(
+      '/api/timeline/following',
+      {
+        method: 'GET',
+        query: {
+          limit: timeline.limit,
+          cursor: timeline.cursor ?? undefined,
+        },
+      },
+    );
+  },
+};
