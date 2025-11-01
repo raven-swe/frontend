fa42be3631afcd32c72d414821767768bc65afea
habibayman <habibaayman2004@outlook.com>
2025-11-01 21:02:30 +0200

    fix: add missing location in user profile

--- app/components/profile/ProfileInfo.vue
+++ app/components/profile/ProfileInfo.vue
@@ -4,39 +4,52 @@ import { formatMonthYear } from '~/utils/date';
 const props = defineProps<{
   userProfile: User;
 }>();
+const displayUsername = '@' + props.userProfile.username;
 const displayUrl = computed(() => {
   if (props.userProfile.websiteUrl) {
     const cleanedUrl = cleanUrl(props.userProfile.websiteUrl);
     return cleanedUrl.length > 30 ? cleanedUrl.slice(0, 29) + '...' : cleanedUrl;
   }
   return '';
-
-  console.log(props.userProfile.bio);
 });
 </script>
 <template>
   <div class="mt-2 flex flex-col">
     <div class="px-4">
-      <h2 class="text-foreground text-2xl font-bold">{{ userProfile.displayName }}</h2>
-      <p class="text-muted-foreground">{{ userProfile.username }}</p>
+      <h2 class="text-foreground pb-0 text-2xl font-bold">{{ userProfile.displayName }}</h2>
+      <p class="text-muted-foreground text-md">{{ displayUsername }}</p>
       <p class="text-muted-foreground mt-2 whitespace-pre-line">{{ userProfile.bio }}</p>
-      <div class="mt-2 flex flex-wrap">
+
+      <div class="mt-2 flex flex-wrap gap-2">
+        <!-- location -->
+        <p
+          v-if="userProfile.location"
+          class="text-muted-foreground flex items-center gap-1 text-sm leading-tight"
+        >
+          <Icon name="ic:sharp-location-on" class="text-muted-foreground" size="18" />
+          {{ userProfile.location }}
+        </p>
+
+        <!-- url -->
         <a
+          v-if="userProfile.websiteUrl"
           :href="userProfile.websiteUrl"
           target="_blank"
           rel="noopener noreferrer"
-          class="text-brand-blue me-2 flex items-center gap-1 hover:underline"
+          class="text-brand-blue me-2 flex items-center gap-1 text-sm hover:underline"
         >
           <Icon class="text-muted-foreground" name="ic:sharp-link" size="18" />
           {{ displayUrl }}
         </a>
 
-        <p class="text-muted-foreground flex items-center gap-1">
+        <!-- join date -->
+        <p class="text-muted-foreground flex items-center gap-1 text-sm">
           <Icon name="ic:sharp-calendar-month" />
           {{ $t('profile-info.joined') }}
           {{ formatMonthYear(userProfile.joinedAt) }}
         </p>
       </div>
+
       <div class="text-muted-foreground mt-4 flex space-x-4">
         <span
           ><strong>{{ userProfile.followingCount }}</strong>
