a1ae0da22060be558a106c6bcafa3893d6120a77
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-02 00:45:15 +0200

    refactor: enhance parameter validation by integrating yup schema in GET /api/tweets/id handler

--- server/api/tweets/[id]/index.get.ts
+++ server/api/tweets/[id]/index.get.ts
@@ -1,8 +1,11 @@
 import type { Tweet } from '~~/shared/types/tweets';
 import { defineWrappedResponseHandler } from '~~/server/utils/handler';
+import * as yup from 'yup';
+
+const paramsSchema = yup.object({ id: yup.string().required().min(1) });
 
 export default defineWrappedResponseHandler(async (event) => {
-  const { id } = event.context.params as { id: string };
+  const { id } = await getValidatedRouterParams(event, (data) => paramsSchema.validate(data));
   const authHeader = getHeader(event, 'authorization');
   return await serverApiFetch<ApiSuccessResponse<Tweet>>(`/tweets/${id}`, {
     method: 'GET',
