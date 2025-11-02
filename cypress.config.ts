fb83ec6305aa45de1ddeb546d09ab270740e331a
Amr Samy <samym5468@gmail.com>
2025-11-02 23:46:40 +0200

    feat: added OTP tests, password validation, and restructured the test suite for extendability

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    env: {
+      API_URL: 'http://localhost:3001', // backend API
+    },
   },
 });
