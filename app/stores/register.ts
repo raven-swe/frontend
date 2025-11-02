dd6495aaabc42e9ccaf6857da4d804da897c106f
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-02 23:47:19 +0200

    fix(register): ensure recaptchaToken is included in registration info

--- app/stores/register.ts
+++ app/stores/register.ts
@@ -8,6 +8,7 @@ export const useRegisterStore = defineStore('register', () => {
     name: '',
     email: '',
     birthDate: '',
+    recaptchaToken: '',
   });
 
   const openDialog = () => {
@@ -21,6 +22,7 @@ export const useRegisterStore = defineStore('register', () => {
       name: '',
       email: '',
       birthDate: '',
+      recaptchaToken: '',
     };
   };
 
