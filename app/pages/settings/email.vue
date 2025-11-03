d92d7ef5145013bb199b9bcd6869b607a83e73dd
Abdallah Farag <146781191+Abdallah-farag27@users.noreply.github.com>
2025-11-03 23:05:11 +0200

    fix: remove unused settings component - [CU-869b1qtqc] (#80)

--- app/pages/settings/email.vue
+++ app/pages/settings/email.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { useChangeEmailStore } from '~/stores/settings/change-email';
-import ChangeEmailForm from './ChangeEmailForm.vue';
-import OtpForm from './OtpForm.vue';
+import ChangeEmailForm from '../../components/emailSettings/ChangeEmailForm.vue';
+import OtpForm from '../../components/emailSettings/OtpForm.vue';
 definePageMeta({
   layout: 'settings',
 });
