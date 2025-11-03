1fedd446e51f987d6e34aa4da00b1c3fd99a4d88
Hussein Mohamed <149144312+Hussein-Mohamed1@users.noreply.github.com>
2025-11-03 14:00:43 +0200

    feat(layouts): add settings page layout - [CU-869avxr9m] (#51)

---
+++ app/layouts/settings.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="bg-background">
+      <div class="flex min-h-screen justify-center">
+        <div class="flex w-full max-w-7xl sm:justify-center">
+          <!-- Left sidebar -->
+          <div class="w-16 flex-shrink-0 sm:w-16 md:w-24 xl:w-[266px] 2xl:w-[206px]">
+            <div class="sticky top-0">
+              <SideBarLeft />
+            </div>
+          </div>
+
+          <!-- settings Section -->
+          <SettingsSettingsSection
+            class="hidden w-[320px] flex-shrink-0 border lg:block xl:w-[390px] 2xl:w-[450px]"
+          />
+
+          <!-- Right sidebar -->
+          <div class="flex-1 border sm:w-[560px] sm:flex-none md:w-[600px]">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
