6ac1575e10a1d92f53510601bce495c329083e84
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-18 03:44:17 +0200

    fix: validate username parameter in profile fetch handler

--- server/api/users/[username]/profile/index.get.ts
+++ server/api/users/[username]/profile/index.get.ts
@@ -1,8 +1,11 @@
 import { defineWrappedResponseHandler } from '~~/server/utils/handler';
 
+import * as yup from 'yup';
+
+const paramsSchema = yup.object({ username: yup.string().required().min(3) });
+
 export default defineWrappedResponseHandler(async (event) => {
-  const params = event.context.params;
-  const username = params?.username as string;
+  const { username } = await getValidatedRouterParams(event, (data) => paramsSchema.validate(data));
   const fetcher = serverApiFetch(event);
 
   const response = await fetcher<ApiSuccessResponse<User>>(`/users/${username}/profile`, {
