908b86c3e77c5f005dddeaf9bff250f6870ff10b
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-18 03:10:27 +0200

    fix: update all server api tests to use new serverApiFetch

--- test/nuxt/server/api/auth/login.post.spec.ts
+++ test/nuxt/server/api/auth/login.post.spec.ts
@@ -7,7 +7,9 @@ import * as jwt from 'jsonwebtoken';
 const h3 = useH3TestUtils();
 
 const mockServerApiFetchRaw = vi.fn();
-vi.stubGlobal('serverApiFetch', { raw: mockServerApiFetchRaw });
+vi.stubGlobal('serverApiFetch', () => ({
+  raw: mockServerApiFetchRaw,
+}));
 
 describe('server/api/auth/login.post', () => {
   beforeEach(() => {
