d0e8aa1927d0444458123511c2d5f380c85dbc80
Mostafa Hassan <moustafahassan028@gmail.com>
2025-11-20 19:48:20 +0200

    refactor: simplify password field layout and button positioning in FieldPassword component

--- app/components/ui/form/FieldPassword.vue
+++ app/components/ui/form/FieldPassword.vue
@@ -11,27 +11,29 @@ const togglePassword = () => (showPassword.value = !showPassword.value);
 </script>
 
 <template>
-  <div class="relative w-full">
-    <UiFormFieldInput
-      v-model="value"
-      :name="props.name"
-      :placeholder="props.placeholder ?? $t('root.auth.password')"
-      :type="showPassword ? 'text' : 'password'"
-      :aria-invalid="!!errorMessage"
-    />
+  <div class="w-full">
+    <div class="relative">
+      <UiFormFieldInput
+        v-model="value"
+        :name="props.name"
+        :placeholder="props.placeholder ?? $t('root.auth.password')"
+        :type="showPassword ? 'text' : 'password'"
+        :aria-invalid="!!errorMessage"
+      />
+
+      <UiButton
+        type="button"
+        class="text-muted-foreground absolute end-3 top-3 flex"
+        variant="ghost-default"
+        size="icon-sm"
+        @mousedown.prevent="togglePassword"
+      >
+        <Icon :name="showPassword ? 'line-md:watch-off' : 'line-md:watch'" size="20" />
+      </UiButton>
+    </div>
 
     <p v-if="errorMessage" class="text-destructive mt-1 ps-1 text-xs">
       {{ errorMessage }}
     </p>
-
-    <UiButton
-      type="button"
-      class="text-muted-foreground absolute end-3 top-1/2 flex -translate-y-1/2"
-      variant="ghost-default"
-      size="icon-sm"
-      @mousedown.prevent="togglePassword"
-    >
-      <Icon :name="showPassword ? 'line-md:watch-off' : 'line-md:watch'" size="20" />
-    </UiButton>
   </div>
 </template>
