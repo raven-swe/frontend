9acee5ed0979c3b56e385dd99e9a6f6c9d1bd48b
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-20 06:27:17 +0200

    feat: update User type to add relationship and enforce follower and following count

--- shared/types/user.ts
+++ shared/types/user.ts
@@ -18,10 +18,17 @@ export type User = {
   websiteUrl: string;
   birthDate: string;
   joinedAt: string; // ISO date string
+  relationship: {
+    blocking: boolean;
+    blockedBy: boolean;
+    muted: boolean;
+    following: boolean;
+    follower: boolean;
+  };
   email: string;
   phone: string;
-  followingCount?: number;
-  followersCount?: number;
+  followingCount: number;
+  followersCount: number;
   mutualsCount?: number;
   mutualNames?: string[];
   languageCode: string;
