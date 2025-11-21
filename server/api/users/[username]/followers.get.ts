ff36074b65f69839eab5cf7165339a9380a2c5a1
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-21 04:02:51 +0200

    feat: implement user followers and following API endpoints with validation

--- server/api/users/[username]/followers.get.ts
+++ server/api/users/[username]/followers.get.ts
@@ -7,10 +7,8 @@ export default defineWrappedResponseHandler(async (event) => {
   );
   const fetcher = serverApiFetch(event);
 
-  const response = await fetcher<ApiSuccessResponse<User>>(`/users/${username}/profile`, {
+  const response = await fetcher<ApiSuccessResponse<User[]>>(`/users/${username}/followers`, {
     method: 'GET',
   });
-
-  console.log(response.data);
   return response;
 });
--- server/api/users/[username]/following.delete.ts
+++ server/api/users/[username]/following.delete.ts
@@ -7,10 +7,8 @@ export default defineWrappedResponseHandler(async (event) => {
   );
   const fetcher = serverApiFetch(event);
 
-  const response = await fetcher<ApiSuccessResponse<User>>(`/users/${username}/profile`, {
-    method: 'GET',
+  const response = await fetcher<ApiResponseBase>(`/users/${username}/following`, {
+    method: 'DELETE',
   });
-
-  console.log(response.data);
   return response;
 });
