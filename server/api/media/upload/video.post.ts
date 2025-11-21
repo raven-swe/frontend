19ec3d0939d500959c58c6cf8eaa39118298d3ba
habibayman <habibaayman2004@outlook.com>
2025-11-21 12:25:15 +0200

    feat(tweet): create ms nuxt endpoint for tweet creation

---
+++ server/api/media/upload/video.post.ts
@@ -0,0 +1,19 @@
+import type { ApiSuccessResponse } from '~~/shared/types/api';
+
+export default defineWrappedResponseHandler(async (event) => {
+  const authHeader = getHeader(event, 'authorization');
+
+  const response = await serverApiFetch<ApiSuccessResponse<{ success: boolean; message: string }>>(
+    `/media/upload/video`,
+    {
+      method: 'POST',
+      body: event.node.req,
+      headers: {
+        authorization: authHeader || '',
+        'content-type': event.node.req.headers['content-type']!,
+      },
+    },
+  );
+
+  return response;
+});
