d485c71daef83197f517fcd8dfc0c44c4df7880d
Mostafa Hassan <moustafahassan028@gmail.com>
2025-11-26 01:51:20 +0200

    feat(searchbar): add SearchBar component

---
+++ app/components/ui/SearchBar.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import { computed, ref, nextTick } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+interface Props {
+  modelValue?: string;
+  placeholder?: string;
+  isFocused?: boolean;
+}
+
+interface Emits {
+  (e: 'update:modelValue', value: string): void;
+  (e: 'update:isFocused', value: boolean): void;
+  (e: 'submit'): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: '',
+  placeholder: '',
+  isFocused: false,
+});
+
+const emit = defineEmits<Emits>();
+const isFocused = ref(props.isFocused);
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const { t } = useI18n();
+const placeholderText = computed(() => props.placeholder || t('ui.search.searchbar.placeholder'));
+
+function handleInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emit('update:modelValue', target.value);
+}
+
+function handleFocus() {
+  isFocused.value = true;
+  emit('update:isFocused', true);
+}
+
+function handleBlur() {
+  isFocused.value = false;
+  emit('update:isFocused', false);
+}
+
+function clearInput() {
+  emit('update:modelValue', '');
+  nextTick(() => {
+    inputRef.value?.focus();
+  });
+}
+
+function handleSubmit() {
+  emit('submit');
+}
+</script>
+
+<template>
+  <div
+    :class="[
+      'relative mb-0 flex w-full items-center gap-1 rounded-full border border-2 px-4 py-3',
+      isFocused ? 'border-primary shadow-accent shadow-md' : '',
+    ]"
+  >
+    <Icon size="20" name="ic:outline-search" class="text-muted-foreground" />
+    <input
+      ref="inputRef"
+      :value="modelValue || ''"
+      type="text"
+      :placeholder="placeholderText"
+      class="h-[15px] flex-1 border-none bg-transparent text-[15px] outline-none"
+      @input="handleInput"
+      @focus="handleFocus"
+      @blur="handleBlur"
+      @keydown.enter="handleSubmit"
+    />
+    <UiButton
+      v-if="modelValue"
+      variant="icon"
+      size="icon"
+      @click="
+        emit('update:modelValue', '');
+        isFocused = true;
+      "
+    >
+      <Icon
+        v-if="modelValue"
+        size="20"
+        name="zondicons:close-solid"
+        class="text-foreground cursor-pointer"
+        @mousedown.prevent="clearInput"
+      />
+    </UiButton>
+  </div>
+</template>
