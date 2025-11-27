88703c9fe6c5938a810adfda1dda049c3a7897df
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-28 00:33:34 +0200

    feat: add BlockToggleButton and MuteToggleButton components for user relationship management

---
+++ app/components/ui/MuteToggleButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <UiButton
+    v-if="relationship?.muted"
+    variant="outline-destructive"
+    class="bg-transparent"
+    size="icon-sm"
+    @click.prevent.stop="$emit('unmute')"
+  >
+    <Icon name="lucide:volume-off" />
+  </UiButton>
+  <UiButton
+    v-else
+    variant="outline-primary"
+    class="bg-transparent"
+    size="icon-sm"
+    @click.prevent.stop="$emit('mute')"
+  >
+    <Icon name="lucide:volume" />
+  </UiButton>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  relationship: User['relationship'];
+}>();
+
+defineEmits<{
+  (e: 'mute' | 'unmute'): void;
+}>();
+</script>
