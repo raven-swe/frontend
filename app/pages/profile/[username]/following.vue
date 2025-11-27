3110009c8b9563405050592cb7d2758992a90fd1
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-27 17:59:13 +0200

    feat: implement infinite query for following users list to match followers list

--- app/pages/profile/[username]/following.vue
+++ app/pages/profile/[username]/following.vue
@@ -1,25 +1,59 @@
 <script lang="ts" setup>
-import { useQuery } from '@tanstack/vue-query';
+import { useInfiniteQuery } from '@tanstack/vue-query';
 import { apiFetch } from '~/api';
 import UserRow from '~/components/ui/UserRow.vue';
+
 definePageMeta({
   layout: 'follower-following',
 });
-
 const router = useRouter();
-const username = computed(() => router.currentRoute.value.params.username?.toString());
-const queryKey = computed(() => ['following', username.value]);
-const { data: users } = useQuery({
-  queryKey,
-  queryFn: async () => {
-    const response = await apiFetch(`/api/users/${username.value}/following`);
-    return response.data;
-  },
+const username = computed(() => {
+  const val = router.currentRoute.value.params.username;
+  return typeof val === 'string' ? val.toLowerCase() : null;
 });
+
+const queryKey = computed(() => ['user-list', username.value, 'following']);
+const { data: usersPaginated } = useInfiniteQuery({
+  queryKey,
+  initialPageParam: null as string | null,
+  queryFn: async ({ signal, pageParam }) => {
+    const response = await apiFetch(`/api/users/${username.value}/following`, {
+      signal,
+      query: {
+        cursor: pageParam,
+      },
+    });
+    return response;
+  },
+  getNextPageParam: (lastPage) =>
+    lastPage.pagination?.hasNextPage ? lastPage.pagination.nextCursor : undefined,
+  structuralSharing: false,
+});
+
+const users = computed(() => usersPaginated.value?.pages.flatMap((page) => page.data) || []);
+const { mutate: followUser } = useFollowMutation(username.value || '');
+
+const handleFollow = (targetUsername: string, action: 'follow' | 'unfollow') => {
+  followUser({ username: targetUsername, action });
+};
 </script>
 
 <template>
   <div>
-    <UserRow v-for="user in users" :key="user.username" :user="user" />
+    <UserRow
+      v-for="user in users"
+      :key="user.username"
+      :user="user"
+      @follow="
+        (username) => {
+          handleFollow(username, 'follow');
+        }
+      "
+      @unfollow="
+        (username) => {
+          handleFollow(username, 'unfollow');
+        }
+      "
+    />
   </div>
 </template>
