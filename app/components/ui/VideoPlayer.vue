5d98fb0e74055f0a2d7b6c49f639167cee9fb157
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-11-27 20:00:17 +0200

    fix: properly integrate videojs in nuxt with plugin

--- app/components/ui/VideoPlayer.vue
+++ app/components/ui/VideoPlayer.vue
@@ -2,28 +2,11 @@
 // Import the Vue wrapper
 import { VideoPlayer } from '@videojs-player/vue';
 
-// Import the default videojs to access Player type
-import type videojs from 'video.js';
-
-import 'video.js/dist/video-js.css';
-
 interface Props {
   src: string;
   poster?: string;
 }
-
 const props = defineProps<Props>();
-type VideoJsPlayer = ReturnType<typeof videojs>;
-
-function onPlayerReady(player: VideoJsPlayer) {
-  player.on('click', () => {
-    if (player.paused()) {
-      player.play();
-    } else {
-      player.pause();
-    }
-  });
-}
 </script>
 
 <template>
@@ -44,6 +27,5 @@ function onPlayerReady(player: VideoJsPlayer) {
         },
       ],
     }"
-    @mounted="onPlayerReady"
   />
 </template>
