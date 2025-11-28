4f8958afc4cb03ae99cbaa7e61e323fd54cf7c8f
Mostafa Hassan <moustafahassan028@gmail.com>
2025-11-28 20:34:28 +0200

    feat(explore): add Hashtag component to display trending hashtags

---
+++ app/components/explore/Hashtag.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { TrendingHashtag } from '~~/shared/types/hashtag';
+
+const props = defineProps<{
+  hashtag: TrendingHashtag;
+}>();
+</script>
+
+<template>
+  <div class="hover:bg-accent flex cursor-pointer flex-col gap-1 p-4 font-bold">
+    <p class="text-xs text-gray-500">{{ $t('Trending in Egypt') }}</p>
+    <p class="text-sm">{{ props.hashtag.hashtag }}</p>
+    <p class="text-xs text-gray-500">{{ $n(props.hashtag.tweetsCount) }} {{ $t('posts') }}</p>
+  </div>
+</template>
