946e57aa7be1b0b5c4c11e93e023f6502df11625
Mostafa Hassan <moustafahassan028@gmail.com>
2025-11-28 20:35:35 +0200

    feat(explore): create explore layout with tabs for navigation

---
+++ app/layouts/explore.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import Tabs from '@/components/ui/Tabs.vue';
+import Tab from '@/components/ui/Tab.vue';
+
+const tabs = [
+  {
+    label: $t('explore.for-you.tab'),
+    route: '/explore/for-you',
+  },
+  {
+    label: $t('explore.trending.tab'),
+    route: '/explore/trending',
+  },
+  {
+    label: $t('explore.news.tab'),
+    route: '/explore/news',
+  },
+  {
+    label: $t('explore.sports.tab'),
+    route: '/explore/sports',
+  },
+  {
+    label: $t('explore.entertainment.tab'),
+    route: '/explore/entertainment',
+  },
+];
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div
+      class="bg-background/60 fixed top-0 z-50 flex inline-flex h-30 w-full max-w-[598px] cursor-pointer flex-col items-center gap-2 rounded-b-md py-1 text-sm font-medium backdrop-blur-sm"
+    >
+      <UiSearchField />
+      <Tabs>
+        <Tab
+          v-for="tab in tabs"
+          :key="tab.route"
+          :label="tab.label"
+          :route="tab.route"
+          :is-active="$route.path === tab.route"
+        />
+      </Tabs>
+    </div>
+    <slot />
+  </NuxtLayout>
+</template>
