caa7e33f548c59ce40ce0317bed3ce127ec09272
Mostafa Hassan <moustafahassan028@gmail.com>
2025-11-28 20:49:50 +0200

    feat(explore): add search layout with tabs for navigation

---
+++ app/layouts/search.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import Tabs from '@/components/ui/Tabs.vue';
+import Tab from '@/components/ui/Tab.vue';
+
+const tabs = [
+  {
+    label: $t('search.top.tab'),
+    route: '/search/top',
+  },
+  {
+    label: $t('search.latest.tab'),
+    route: '/search/latest',
+  },
+  {
+    label: $t('search.people.tab'),
+    route: '/search/people',
+  },
+  {
+    label: $t('search.media.tab'),
+    route: '/search/media',
+  },
+];
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div
+      class="bg-background/60 fixed top-0 z-50 flex inline-flex h-30 w-full max-w-[598px] cursor-pointer flex-col items-center gap-2 rounded-b-md py-1 text-sm font-medium backdrop-blur-sm"
+    >
+      <UiSearchField />
+      <Tabs>
+        <Tab
+          v-for="tab in tabs"
+          :key="tab.route"
+          :label="tab.label"
+          :route="tab.route"
+          :is-active="$route.path === tab.route"
+        />
+      </Tabs>
+    </div>
+    <slot />
+  </NuxtLayout>
+</template>
---
+++ app/services/search/searchService.ts
@@ -0,0 +1,52 @@
+import { apiFetch } from '~/api';
+import type { PaginationParams } from '~~/shared/types/pagination';
+import type { validSearchTweetsTabs } from '~~/shared/types/timeline';
+
+export const exploreService = {
+  async getTopThreeHashtags(query: string) {
+    return await apiFetch(`/api/search/hashtags/top`, {
+      method: 'GET',
+      query: {
+        query: query,
+      },
+    });
+  },
+
+  async search(query: string) {
+    const usersResponse = await this.getPeople({ cursor: null, limit: 10 }, query);
+    const users = usersResponse.data;
+    const hashtagsResponse = await this.getTopThreeHashtags(query);
+    const hashtags: [string] = hashtagsResponse.data;
+    return {
+      users,
+      hashtags,
+    };
+  },
+
+  async getTweets(
+    pagination: PaginationParams,
+    query: string,
+    tab: (typeof validSearchTweetsTabs)[number],
+  ) {
+    return apiFetch(`/api/search/tweets`, {
+      method: 'GET',
+      query: {
+        query: query,
+        tab: tab,
+        limit: pagination.limit,
+        cursor: pagination.cursor ?? undefined,
+      },
+    });
+  },
+
+  async getPeople(pagination: PaginationParams, query: string) {
+    return apiFetch(`/api/search/users/all`, {
+      method: 'GET',
+      query: {
+        query: query,
+        limit: pagination.limit,
+        cursor: pagination.cursor ?? undefined,
+      },
+    });
+  },
+};
