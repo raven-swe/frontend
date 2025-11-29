288a5a8750bb0624fe09a5c4e227195ac02b5bd5
Abdallah Farag <146781191+Abdallah-farag27@users.noreply.github.com>
2025-11-29 18:29:35 +0200

    fix: update app/components/tweet/TweetDefaultCard.vue
    Co-authored-by: Habiba Ayman <137416623+habibayman@users.noreply.github.com>

--- app/components/tweet/TweetDefaultCard.vue
+++ app/components/tweet/TweetDefaultCard.vue
@@ -143,14 +143,14 @@ function handleTweetClick() {
           <span v-if="seg.type === 'text'" class="inline">
             {{ seg.text }}
           </span>
-          <a
-            v-else
-            :href="seg.href"
-            class="text-primary inline font-medium hover:underline"
-            @click.stop
-          >
-            {{ seg.text }}
-          </a>
+    <NuxtLink
+      v-else
+      :to="seg.href"
+      class="text-primary inline font-medium hover:underline"
+      @click.stop
+    >
+      {{ seg.text }}
+    </NuxtLink>
         </template>
       </p>
 
