4d36057a78bc447813d9250e9496c8797d9597de
Mostafa Hassan <moustafahassan028@gmail.com>
2025-12-02 12:52:19 +0200

    feat(explore): add Hashtag component for search navigation

---
+++ app/components/search/Hashtag.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  hashtag: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="`/search/top?q=${encodeURIComponent(props.hashtag)}`"
+    class="hover:bg-accent block cursor-pointer p-4"
+  >
+    <div class="flex gap-1">
+      <div>
+        <Icon size="20" name="ic:outline-search" class="text-muted-foreground" />
+      </div>
+      <div>
+        <p class="text-sm font-bold">{{ props.hashtag }}</p>
+        <p class="text-muted-foreground text-xs">{{ $t('explore.trending.tab') }}</p>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
