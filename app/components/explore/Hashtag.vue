ad6fae6694ac61d891cba15bb3c5afa859ce344f
Mostafa Hassan <moustafahassan028@gmail.com>
2025-12-02 12:46:22 +0200

    feat(explore): update Hashtag component to use NuxtLink for navigation

--- app/components/explore/Hashtag.vue
+++ app/components/explore/Hashtag.vue
@@ -4,12 +4,20 @@ import type { TrendingHashtag } from '~~/shared/types/hashtag';
 const props = defineProps<{
   hashtag: TrendingHashtag;
 }>();
+
+const handleClick = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
 </script>
 
 <template>
-  <div class="hover:bg-accent flex cursor-pointer flex-col gap-1 p-4 font-bold">
+  <NuxtLink
+    :to="`/search/top?q=${encodeURIComponent('#' + props.hashtag.hashtag)}`"
+    class="hover:bg-accent flex cursor-pointer flex-col gap-1 p-4 font-bold"
+    @click="handleClick"
+  >
     <p class="text-xs text-gray-500">{{ $t('Trending in Egypt') }}</p>
     <p class="text-sm">{{ props.hashtag.hashtag }}</p>
     <p class="text-xs text-gray-500">{{ $n(props.hashtag.tweetsCount) }} {{ $t('posts') }}</p>
-  </div>
+  </NuxtLink>
 </template>
