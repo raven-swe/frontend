c1d749c58889aaa9b0e4f0e8d042b8ba5daef17d
habibayman <habibaayman2004@outlook.com>
2025-12-02 23:23:43 +0200

    refactor: remove skin color changing emojis

--- app/components/ui/emoji-picker/emoji-picker-footer.vue
+++ app/components/ui/emoji-picker/emoji-picker-footer.vue
@@ -2,21 +2,6 @@
 import type { HTMLAttributes } from 'vue';
 import EmojiPickerPrimitive from 'vue-frimousse';
 import { cn } from '@/utils/index';
-import { cva } from 'class-variance-authority';
-
-const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50',
-  {
-    variants: {
-      variant: {
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-      },
-      size: {
-        icon: 'h-9 w-9',
-      },
-    },
-  },
-);
 
 interface Props {
   class?: HTMLAttributes['class'];
@@ -51,10 +36,6 @@ const props = defineProps<Props>();
             {{ $t('tweet.composer.select-emoji') }}
           </span>
         </template>
-        <EmojiPickerPrimitive.SkinToneSelector
-          :class="cn(buttonVariants({ variant: 'secondary', size: 'icon' }), 'size-7 rounded-md')"
-          :title="$t('tweet.composer.change-skin-tone')"
-        />
       </div>
     </EmojiPickerPrimitive.ActiveEmoji>
   </div>
