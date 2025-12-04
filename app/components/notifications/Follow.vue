039904df28cbce52aa520af4c86f9594bfedcb4d
habibayman <habibaayman2004@outlook.com>
2025-12-04 05:22:25 +0200

    feat: implement like&follow notifications

---
+++ app/components/notifications/Follow.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  timestamp: string;
+  actor: ActorSummary;
+  isSeen?: boolean;
+}>();
+
+const icon = {
+  name: 'lucide:user-plus',
+  color: 'text-brand-blue',
+};
+
+const linkTo = `/profile/@${props.actor.username}`;
+
+const message = $t('notifications.message.follow');
+</script>
+
+<template>
+  <NotificationsBase
+    :type="'FOLLOW'"
+    :message="message"
+    :timestamp="props.timestamp"
+    :icon="icon"
+    :actor="props.actor"
+    :link-to="linkTo"
+    :is-seen="props.isSeen"
+  />
+</template>
---
+++ app/components/notifications/Like.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { Tweet } from '~~/shared/types/tweets';
+import TweetDefaultCard from '../tweet/TweetDefaultCard.vue';
+
+const props = defineProps<{
+  timestamp: string;
+  actor: ActorSummary;
+  isSeen?: boolean;
+  tweet: Tweet;
+}>();
+
+const icon = {
+  name: 'lucide:heart',
+  color: 'text-brand-red',
+};
+
+const linkTo = `/profile/${props.tweet.author.username}}/status/${props.tweet.id}`;
+
+const message = $t('notifications.message.like');
+</script>
+
+<template>
+  <NotificationsBase
+    :type="'LIKE'"
+    :message="message"
+    :timestamp="props.timestamp"
+    :icon="icon"
+    :actor="props.actor"
+    :link-to="linkTo"
+    :is-seen="props.isSeen"
+  >
+    <TweetDefaultCard :tweet="props.tweet" class="mt-2" />
+  </NotificationsBase>
+</template>
