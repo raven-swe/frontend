faf95916074340f66bd9c031ad3d08d5c3f725c7
Ahmed Amr <ahmedamr24680@gmail.com>
2025-12-05 01:30:01 +0200

    test: add unit test for AccountSetup

--- app/components/profile/account-setup/index.vue
+++ app/components/profile/account-setup/index.vue
@@ -2,13 +2,14 @@
 import EditUsernameDialog from '~/components/profile/account-setup/EditUsernameDialog.vue';
 import FollowUserDialog from '~/components/profile/account-setup/FollowUserDialog.vue';
 import SelectInterestsDialog from '~/components/profile/account-setup/SelectInterestsDialog.vue';
+import ProfilePictureDialog from '~/components/profile/setup/ProfilePictureDialog.vue';
 
 const { setupStep, handleProfilePictureSubmit } = useAccountSetup();
 </script>
 
 <template>
   <div>
-    <ProfileSetupProfilePictureDialog
+    <ProfilePictureDialog
       :open="setupStep === 'profile-picture'"
       disable-close
       @submit="handleProfilePictureSubmit"
@@ -18,5 +19,3 @@ const { setupStep, handleProfilePictureSubmit } = useAccountSetup();
     <FollowUserDialog :open="setupStep === 'follow-user'" />
   </div>
 </template>
-
-<style></style>
