865782b7f7d00747262309e0dcb54f0f6968c976
Loay Ahmed <loayahmed304@gmail.com>
2025-12-05 16:38:10 +0200

    feat: pass user-agent header to backend - [CU-869bd6brf] (#155)

--- server/utils/api.ts
+++ server/utils/api.ts
@@ -20,6 +20,9 @@ export const serverApiFetch = (event: H3Event) =>
 
         const xForwardedIp = getHeader(event, 'X-Forwarded-For');
         if (xForwardedIp) options.headers.append('X-Forwarded-For', xForwardedIp);
+
+        const userAgent = getHeader(event, 'User-Agent');
+        if (userAgent) options.headers.append('User-Agent', userAgent);
       }
       // Ensure the X-Client-Type header is set to web
       options.headers.append('X-Client-Type', 'web');
