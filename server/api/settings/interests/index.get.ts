98687d3af69fb0f06cd3c44e1af9376421256117
Ahmed Amr <ahmedamr24680@gmail.com>
2025-12-05 04:14:13 +0200

    test: add unit tests for GET and PUT /settings/interests endpoints

--- server/api/settings/interests/index.get.ts
+++ server/api/settings/interests/index.get.ts
@@ -1,3 +1,5 @@
+import { defineWrappedResponseHandler } from '~~/server/utils/handler';
+
 export default defineWrappedResponseHandler(async (event) => {
   const fetcher = serverApiFetch(event);
   return await fetcher<ApiSuccessResponse<Interest[]>>(`/me/settings/interests`, {
