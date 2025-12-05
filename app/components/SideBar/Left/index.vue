70f52d14bda57fe7729a64bb0c98e41489deb147
Ahmed Amr <ahmedamr24680@gmail.com>
2025-12-05 21:55:09 +0200

    feat: enhance logout functionality with confirmation dialog and update translations

--- app/components/SideBar/Left/index.vue
+++ app/components/SideBar/Left/index.vue
@@ -14,7 +14,7 @@ const queryClient = useQueryClient();
 
 const handleLogout = async () => {
   await loginService.logout();
-  queryClient.removeQueries({ queryKey: ['layout-data'] });
+  await queryClient.clear();
 };
 
 const lang = ref(locale.value);
@@ -71,38 +71,61 @@ const switchLanguage = () => {
       </UiButton>
     </div>
     <div class="flex w-full flex-grow p-2 pb-4">
-      <UiDropdownMenu>
-        <UiDropdownMenuTrigger as-child>
-          <UiButton
-            variant="ghost-default"
-            size="2xl"
-            class="mx-auto mt-auto xl:w-full"
-            data-cy="logout-btn-trigger"
-          >
-            <div class="flex w-full items-center gap-3">
-              <Avatar
-                :img="userStore.user?.avatarUrl || ''"
-                :alt="userStore.user?.displayName || 'User Avatar'"
-                size="sm"
-              />
-              <div class="hidden text-start xl:block">
-                <p>{{ userStore.user?.displayName || 'User' }}</p>
-                <p class="text-muted-foreground text-sm">
-                  {{ '@' + (userStore.user?.username || 'username') }}
-                </p>
+      <UiAlertDialog>
+        <UiDropdownMenu>
+          <UiDropdownMenuTrigger as-child>
+            <UiButton
+              variant="ghost-default"
+              size="2xl"
+              class="mx-auto mt-auto xl:w-full"
+              data-cy="logout-btn-trigger"
+            >
+              <div class="flex w-full items-center gap-3">
+                <Avatar
+                  :img="userStore.user?.avatarUrl || ''"
+                  :alt="userStore.user?.displayName || 'User Avatar'"
+                  size="sm"
+                />
+                <div class="hidden text-start xl:block">
+                  <p>{{ userStore.user?.displayName || 'User' }}</p>
+                  <p class="text-muted-foreground text-sm">
+                    {{ '@' + (userStore.user?.username || 'username') }}
+                  </p>
+                </div>
+                <div class="ms-auto hidden xl:flex">
+                  <Icon name="lucide:more-horizontal" class="pe-2" />
+                </div>
               </div>
-              <div class="ms-auto hidden xl:flex">
-                <Icon name="lucide:more-horizontal" class="pe-2" />
-              </div>
-            </div>
-          </UiButton>
-        </UiDropdownMenuTrigger>
-        <UiDropdownMenuContent align="center" class="bg-background">
-          <UiDropdownMenuItem data-cy="logout-button" @click="handleLogout">
-            {{ $t('ui.logout', { username: userStore.user?.username || 'username' }) }}
-          </UiDropdownMenuItem>
-        </UiDropdownMenuContent>
-      </UiDropdownMenu>
+            </UiButton>
+          </UiDropdownMenuTrigger>
+          <UiDropdownMenuContent align="center" class="bg-background">
+            <UiAlertDialogTrigger>
+              <UiDropdownMenuItem data-cy="logout-button">
+                {{ $t('ui.logout.label', { username: userStore.user?.username || 'username' }) }}
+              </UiDropdownMenuItem>
+            </UiAlertDialogTrigger>
+          </UiDropdownMenuContent>
+        </UiDropdownMenu>
+
+        <UiAlertDialogContent>
+          <UiAlertDialogHeader>
+            <UiAlertDialogTitle>
+              {{ $t('ui.logout.title') }}
+            </UiAlertDialogTitle>
+            <UiAlertDialogDescription>
+              {{ $t('ui.logout.description') }}
+            </UiAlertDialogDescription>
+          </UiAlertDialogHeader>
+          <UiAlertDialogFooter>
+            <UiAlertDialogAction data-cy="confirm-logout-button" @click="handleLogout">
+              {{ $t('ui.logout.confirm') }}
+            </UiAlertDialogAction>
+            <UiAlertDialogCancel>
+              {{ $t('ui.cancel') }}
+            </UiAlertDialogCancel>
+          </UiAlertDialogFooter>
+        </UiAlertDialogContent>
+      </UiAlertDialog>
     </div>
   </div>
 </template>
