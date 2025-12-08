35fc3d9ce509968a112f2ec77ebdbe4b3c2c0ae8
Abdallah-farag27 <abdallah_altalbawy@yahoo.com>
2025-12-08 19:10:36 +0200

    feat: add locale parameter to tweet AI summary for localization support

--- app/services/tweet/tweetsService.ts
+++ app/services/tweet/tweetsService.ts
@@ -25,11 +25,14 @@ export const tweetsService = {
   },
 };
 
-export const tweetAiSummary = async (id: string) => {
+export const tweetAiSummary = async (id: string, locale: string) => {
   return await apiFetch<ApiSuccessResponse<{ id: string; summary: string }>>(
     `/api/tweets/${id}/summary`,
     {
       method: 'GET',
+      query: {
+        locale: locale,
+      },
     },
   );
 };
--- server/api/tweets/[id]/summary/index.get.ts
+++ server/api/tweets/[id]/summary/index.get.ts
@@ -3,14 +3,17 @@ import * as yup from 'yup';
 const paramsSchema = yup.object({ id: yup.string().required().min(1) });
 export default defineWrappedResponseHandler(async (event) => {
   const { id } = await getValidatedRouterParams(event, (data) => paramsSchema.validate(data));
-  // const body = getQuery<{
-  //   baseUsername: string;
-  // }>(event);
+  const query = getQuery<{
+    locale: string;
+  }>(event);
   const fetcher = serverApiFetch(event);
   const response = await fetcher<ApiSuccessResponse<{ id: string; summary: string }>>(
     `/tweets/${id}/summary`,
     {
       method: 'GET',
+      query: {
+        locale: query.locale,
+      },
     },
   );
   return response;
