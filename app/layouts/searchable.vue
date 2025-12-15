5173e44a75838d187f16da3a64777b8dabd3a5d5
Mostafa Hassan <moustafahassan028@gmail.com>
2025-12-15 14:07:00 +0200

    fix: update button variants and improve layout responsiveness

--- app/layouts/searchable.vue
+++ app/layouts/searchable.vue
@@ -21,15 +21,20 @@ const openSearchSettings = () => {
 <template>
   <NuxtLayout name="default">
     <div
-      class="bg-background/60 sticky top-0 z-50 flex inline-flex h-30 w-full max-w-[598px] cursor-pointer flex-col items-center gap-2 rounded-b-md py-1 text-sm font-medium backdrop-blur-sm"
+      class="bg-background/60 sticky top-0 z-50 flex w-full cursor-pointer flex-col items-center gap-2 rounded-b-md py-1 text-sm font-medium backdrop-blur-sm"
     >
       <div class="flex w-full items-center justify-between px-2">
-        <UiButton v-if="!isExplore" variant="icon" size="icon" @click="goBack">
-          <Icon class="mx-3" :name="$t('icons.back-button-icon')" size="1.3rem" />
+        <UiButton v-if="!isExplore" variant="ghost-default" size="icon-lg" @click="goBack">
+          <Icon :name="$t('icons.back-button-icon')" size="1.3rem" />
         </UiButton>
         <UiSearchField :show-back-on-focus="isExplore" />
-        <UiButton v-if="!isExplore" variant="icon" size="icon" @click="openSearchSettings">
-          <Icon class="mx-3" name="ic:more-horiz" size="1.3rem" />
+        <UiButton
+          v-if="!isExplore"
+          variant="ghost-default"
+          size="icon-lg"
+          @click="openSearchSettings"
+        >
+          <Icon :name="'ic:more-horiz'" size="1.3rem" />
         </UiButton>
       </div>
       <SearchSettingsDialog v-model:open="showSearchSettings" />
