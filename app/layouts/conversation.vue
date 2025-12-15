1918fd1dbe18df2ff5e7a99c13733883222547e2
Ahmed Amr <ahmedamr24680@gmail.com>
2025-12-15 03:41:31 +0200

    feat: enhance responsiveness of conversation layout

--- app/layouts/conversation.vue
+++ app/layouts/conversation.vue
@@ -2,50 +2,46 @@
 import useMyProfileQuery from '~/composables/useMyProfileQuery';
 useMyProfileQuery();
 
-const props = defineProps<{
+defineProps<{
   hideMiddleOnMobile?: boolean;
   hideRightOnMobile?: boolean;
 }>();
-
-// Compute responsive classes based on props
-const middleClasses = computed(() => {
-  if (props.hideMiddleOnMobile) {
-    return 'hidden h-full w-[320px] flex-shrink-0 border lg:block xl:w-[390px] 2xl:w-[450px]';
-  }
-  return 'h-full lg:w-[320px] flex-1 border sm:w-[560px]  sm:flex-none xl:w-[390px] 2xl:w-[450px]';
-});
-
-const rightClasses = computed(() => {
-  if (props.hideRightOnMobile) {
-    return 'hidden h-full flex-1 border sm:w-[560px] sm:flex-none  lg:block';
-  }
-  return 'h-full flex-1 border sm:w-[560px] sm:flex-none ';
-});
 </script>
 
 <template>
-  <div>
-    <div class="bg-background">
-      <div class="flex h-screen justify-center overflow-hidden">
-        <div class="flex w-full max-w-7xl sm:justify-center">
-          <!-- Left sidebar -->
-          <div class="w-16 flex-shrink-0 sm:w-16 md:w-24 xl:w-[266px] 2xl:w-[206px]">
-            <div class="sticky top-0">
-              <SideBarLeft />
-            </div>
-          </div>
-
-          <!-- settings Section -->
-          <div :class="middleClasses">
-            <slot name="middle" />
-          </div>
-
-          <!-- Right sidebar -->
-          <div :class="rightClasses">
-            <slot name="right" />
-          </div>
+  <div class="bg-background flex min-h-screen justify-center">
+    <div class="bg-background flex min-h-screen w-full max-w-7xl justify-center">
+      <!-- Left sidebar -->
+      <div class="w-16.5 flex-shrink-0 duration-100 xl:w-70" data-cy="left-sidebar">
+        <div class="top-0 sm:sticky">
+          <SideBarLeft />
         </div>
+      </div>
+
+      <!-- Main content -->
+      <div
+        class="midd:!w-96 h-screen w-150 border-x lg:block lg:w-80"
+        :class="{
+          hidden: hideMiddleOnMobile,
+        }"
+      >
+        <slot name="middle" />
+      </div>
+
+      <div
+        class="h-screen w-150 border-x lg:block"
+        :class="{
+          hidden: hideRightOnMobile,
+        }"
+      >
+        <slot name="right" />
       </div>
     </div>
   </div>
 </template>
+
+<style>
+html {
+  overflow-y: scroll;
+}
+</style>
