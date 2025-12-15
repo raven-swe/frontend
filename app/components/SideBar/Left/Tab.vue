fbfaab54152788e6e950ae6a8f9c59dcd28090bb
habibayman <habibaayman2004@outlook.com>
2025-12-15 14:52:31 +0200

    refactor: remove sound playing from DMs

--- app/components/SideBar/Left/Tab.vue
+++ app/components/SideBar/Left/Tab.vue
@@ -17,10 +17,8 @@ const { play } = useNotificationSound();
 watch(
   () => props.tab.badgeCount,
   (newCount, oldCount) => {
-    // console.log('Badge count changed from', oldCount, 'to', newCount);
     if (oldCount === undefined || newCount === undefined) return;
-    if (newCount > oldCount) {
-      // console.log('Playing notification sound for badge count increase');
+    if (newCount > oldCount && props.tab.route.includes('notifications')) {
       play();
     }
   },
